fbbab1d75a5c86525a879067ae38c5feb374951c
Evaristo.Aguirre <dasbrotchen@gmail.com>
2023-10-15 17:13:30 -0300

    retoque nombre y queries - prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timerDays, setTimerDays] = useState(0);
+  const [timerHours, setTimerHours] = useState(0);
+  const [timerMinutes, setTimerMinutes] = useState(0);
+  const [timerSeconds, setTimerSeconds] = useState(0);
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countDownDate = new Date('October 27, 2023 13:00:00').getTime();
+    console.log('countDownDate:', countDownDate);
+    interval.current = setInterval(() => {
+      const now = new Date().getTime();
+      console.log('now:', now);
+      const distance = countDownDate - now;
+      console.log('distance:', distance);
+
+
+      if (distance > 0) {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+        const minutes = Math.floor((distance % (1000 * 60 * 60) / (1000 * 60)));
+        const seconds = Math.floor((distance % (1000 * 60) / 1000));
+
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      } else {
+        clearInterval(interval.current);
+        setTimerDays(0);
+        setTimerHours(0);
+        setTimerMinutes(0);
+        setTimerSeconds(0);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="fondo">
+        
+        <div className="grid-item">
+          <img src='../img/img1.jpeg' alt="" />
+        </div>
+        <div className="grid-item">
+          <img src='../img/img2.jpeg' alt="" />
+        </div>
+        <div className="grid-item">
+          <img src='../img/img3.jpeg' alt="" /> 
+        </div>
+        <div className="grid-item">
+          <img src='../img/img4.jpeg' alt="" />
+        </div>
+        <div className="grid-item item-contador">
+            <h1>Cataratas, yendo..</h1>
+            <div className="contador">
+              <div>{timerDays} días y {timerHours}:{timerMinutes}:{timerSeconds} hs</div>
+            </div>
+        </div>
+        <div className="grid-item">
+          <img src='../img/img5.jpeg' alt="" />
+        </div>
+        <div className="grid-item">
+          <img src='../img/img6.jpeg' alt="" />
+        </div>
+        <div className="grid-item">
+          <img src='../img/img7.jpeg' alt="" />
+        </div>
+        <div className="grid-item">
+          <img src='../img/img8.jpeg' alt="" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
